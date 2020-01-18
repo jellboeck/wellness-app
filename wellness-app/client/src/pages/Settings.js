125749f61453445ef278aaaa9b407650a7817f89
A Shamrock <53099864+ashamrock@users.noreply.github.com>
2020-01-18 13:57:13 -0500

    Add files via upload

--- wellness-app/client/src/pages/Settings.js
+++ wellness-app/client/src/pages/Settings.js
@@ -2,13 +2,16 @@ import React from "react";
 import { Container, Row, Col } from "shards-react";
 
 import UserAccountDetails from "../components/user-settings/UserSettings";
+import UserInput from "../components/user-settings/UserInputs";
+import SettingsNavbar from "components/Navbars/NavbarSettings.js";
 
 const UserSettings = () => (
   <Container fluid className="main-content-container px-4">
-
+<SettingsNavbar />
     <Row>
       <Col lg="8">
         <UserAccountDetails />
+        <UserInput />
       </Col>
     </Row>
   </Container>
