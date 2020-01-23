5323001994084f603dd6ccd158cb1ec634b8d84e
Grievous Angel <ronfud2000@gmail.com>
2020-01-22 22:50:37 -0500

    use current values in userGoals

--- wellness-app/client/src/pages/Settings.js
+++ wellness-app/client/src/pages/Settings.js
@@ -7,8 +7,9 @@ import UserInput from "../components/user-settings/UserInputs";
 function UserSettings(props) {
   console.log('usersettings');
   console.log(props);
-  return (
-    <Container fluid className="main-content-container px-4">
+
+    return (
+      <Container fluid className="main-content-container px-4">
 
     <Row>
       <Col lg="8">
