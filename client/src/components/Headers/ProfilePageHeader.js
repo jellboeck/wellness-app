4be03ce5175bd51e859955d8a8a8ac58d4ba6e4a
Julia Ellboeck <juliaellboeck@gmail.com>
2020-01-28 19:31:02 -0500

    use effect uncomment

--- client/src/components/Headers/ProfilePageHeader.js
+++ client/src/components/Headers/ProfilePageHeader.js
@@ -37,19 +37,19 @@ function ProfilePageHeader(props) {
 
   getUserProfile();
 
-  // React.useEffect(() => {
-  //   if (window.innerWidth > 991) {
-  //     const updateScroll = () => {
-  //       let windowScrollTop = window.pageYOffset / 3;
-  //       pageHeader.current.style.transform =
-  //         "translate3d(0," + windowScrollTop + "px,0)";
-  //     };
-  //     window.addEventListener("scroll", updateScroll);
-  //     return function cleanup() {
-  //       window.removeEventListener("scroll", updateScroll);
-  //     };
-  //   }
-  // });
+  React.useEffect(() => {
+    if (window.innerWidth > 991) {
+      const updateScroll = () => {
+        let windowScrollTop = window.pageYOffset / 3;
+        pageHeader.current.style.transform =
+          "translate3d(0," + windowScrollTop + "px,0)";
+      };
+      window.addEventListener("scroll", updateScroll);
+      return function cleanup() {
+        window.removeEventListener("scroll", updateScroll);
+      };
+    }
+  });
   return (
     <>
       <div
